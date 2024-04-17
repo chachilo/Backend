f3f606a469735552e6c1e696e3abac9c54ce55a7
Rafael Galindo F <126412980+chachilo@users.noreply.github.com>
2024-04-16 18:52:59 -0600

    Base Datos

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const MONGO_URL = "mongodb+srv://rafael3041220219:Casterfiler25%@cluster0.m9fchhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("connect mongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
